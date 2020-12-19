a4da2668435f2e5678ba0bc0599aa86ec3a64a95
JT Turner <jtwebman@gmail.com>
2020-12-19 14:30:48 -0800

    Fix log format in non production and add timestamp.

--- server/lib/get-logger.js
+++ server/lib/get-logger.js
@@ -5,9 +5,21 @@ const winston = require('winston');
 const pjson = require('../../package.json');
 
 let winstonFormat = winston.format.json();
+let metaData = {
+  app: pjson.name,
+  version: pjson.version,
+  time: new Date().toISOString()
+};
 
 if (process.env.NODE_ENV != 'production') {
-  winstonFormat = winston.format.combine(winston.format.json(), winston.format.prettyPrint());
+  winstonFormat = winston.format.combine(
+    winston.format.json(),
+    winston.format.prettyPrint(),
+    winston.format.colorize()
+  );
+  metaData = {
+    time: new Date().toISOString()
+  };
 }
 
 /**
@@ -17,15 +29,10 @@ if (process.env.NODE_ENV != 'production') {
  */
 function getLogger(config) {
   return winston.createLogger({
-    defaultMeta: {
-      app: pjson.name,
-      version: pjson.version
-    },
+    defaultMeta: metaData,
     transports: [
       new winston.transports.Console({
         level: config.get('LOGGER_LEVEL'),
-        prettyPrint: true,
-        colorize: true,
         format: winstonFormat,
         handleExceptions: true,
         handleRejections: true
