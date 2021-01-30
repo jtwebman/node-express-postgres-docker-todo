5bb32f5b95712910ff020beef48d1f18ab59a4fb
JT Turner <jt.turner@azlo.com>
2021-01-30 12:44:26 -0800

    Fix to deploy container to docker hub.

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,14 @@
 # Get node.js package version
 PACKAGE_VERSION=$(grep '"version":' package.json | cut -d\" -f4)
 
-# Login to docker hub and push latest, dev, and version tags
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+# Build containers dev and production
 docker build -t jtwebman/node-express-postgres-docker-todo:dev --target=development .
 docker build -t jtwebman/node-express-postgres-docker-todo .
+
+# Tag production with package version as well
 docker tag jtwebman/node-express-postgres-docker-todo "jtwebman/node-express-postgres-docker-todo:$PACKAGE_VERSION"
-docker tag jtwebman/node-express-postgres-docker-todo "jtwebman/node-express-postgres-docker-todo:build-$TRAVIS_BUILD_NUMBER"
+
+# Push versions to docker hub
 docker push jtwebman/node-express-postgres-docker-todo:latest
 docker push "jtwebman/node-express-postgres-docker-todo:$PACKAGE_VERSION"
-docker push "jtwebman/node-express-postgres-docker-todo:build-$TRAVIS_BUILD_NUMBER"
 docker push jtwebman/node-express-postgres-docker-todo:dev
