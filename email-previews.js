a447daaa8a44d8334c61bb445692f36c65add4b4
JT Turner <jtwebman@gmail.com>
2023-02-25 20:13:17 -0800

    Add email generation and previews.

---
+++ email-previews.js
@@ -0,0 +1,36 @@
+'use script';
+
+const fs = require('fs');
+const { join } = require('path');
+
+const { generate } = require('./emails/generate');
+
+async function writePreviewFile(name, data) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(join(__dirname, 'emails', 'previews', name), data, error => {
+      if (error) {
+        return reject(error);
+      }
+      return resolve();
+    });
+  });
+}
+
+async function generatePrviews() {
+  return Promise.all([
+    writePreviewFile(
+      'signup.html',
+      await generate('signup', {
+        confirmUrl: 'https://jtwebman.com',
+      }),
+    ),
+  ]);
+}
+
+generatePrviews()
+  .then(() => {
+    console.log(`Previews generated ${join(__dirname, 'emails', 'previews')}`);
+  })
+  .catch(error => {
+    console.log(`Error generating previews ${error}`);
+  });
---
+++ emails/generate.js
@@ -0,0 +1,25 @@
+'use script';
+
+const pug = require('pug');
+const inlineCss = require('inline-css');
+
+const signup = pug.compileFile(`${__dirname}/templates/signup.pug`);
+
+const emails = {
+  signup,
+};
+
+async function generate(name, data) {
+  if (!typeof emails[name] === 'function') {
+    throw new Error(`Email ${name} does not exist`);
+  }
+  const html = emails[name](data);
+  const inlineCessHtml = await inlineCss(html, {
+    url: __dirname,
+  });
+  return inlineCessHtml;
+}
+
+module.exports = {
+  generate,
+};
